1c07df42ba6cabf6917a3881a2d9224ec5be95f5
Mark <Mark@BUBBLE-LAP-01>
2014-10-12 11:35:50 +0100

    Cosmetic: Replace tabs with spaces.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,9 @@
 
 public class HelloWorld
 {
-	public static void main(String[] args)
-	{
-		// Print 'Hello world!'
-		System.out.println("Hello world!");
-	}
+    public static void main(String[] args)
+    {
+        // Print 'Hello world!'
+        System.out.println("Hello world!");
+    }
 }
